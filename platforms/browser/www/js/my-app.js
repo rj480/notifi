ee68d4e0580b7529a7efecf10df24567e877921f
DESKTOP-MRS4S5L\sobedone <rjam480@gmail.com>
2017-05-02 15:45:03 -0500

    algo

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -20,15 +20,9 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
 
-window.plugins.PushbotsPlugin.initialize("5908a7694a9efabe108b4567", {"android":{"sender_id":"1053716922468"}});
+window.plugins.PushbotsPlugin.initialize("5908a7694a9efabe108b4567", {"android":{"sender_id":"1018992769151"}});
 
-window.plugins.PushbotsPlugin.on("notification:received", function(data){
-	console.log("received:" + JSON.stringify(data));
-	
-	//Silent notifications Only [iOS only]
-	//Send CompletionHandler signal with PushBots notification Id
-	window.plugins.PushbotsPlugin.done(data.pb_n_id);
-});
+
 
 });
 
