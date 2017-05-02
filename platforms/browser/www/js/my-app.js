ac95f7c68e3307172ee5394e7d482fefb6a3f96a
DESKTOP-MRS4S5L\sobedone <rjam480@gmail.com>
2017-05-02 14:45:01 -0500

    cambios para las notificaciones

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -19,6 +19,16 @@ var mainView = myApp.addView('.view-main', {
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+
+window.plugins.PushbotsPlugin.initialize("5908a7694a9efabe108b4567", {"android":{"sender_id":"1053716922468"}});
+
+window.plugins.PushbotsPlugin.on("notification:received", function(data){
+	console.log("received:" + JSON.stringify(data));
+	
+	//Silent notifications Only [iOS only]
+	//Send CompletionHandler signal with PushBots notification Id
+	window.plugins.PushbotsPlugin.done(data.pb_n_id);
+});
+
 });
 
